b114df04d2c21d3b7948c7dc0b941528f57faef8
wz xiaoxin <572400957@qq.com>
2023-11-07 17:18:59 +0800

    update

--- weeklycontest/370/d.cpp
+++ weeklycontest/370/d.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class BIT {
+    vector<long long> tree;
+    int n;
+public:
+    BIT(int n) {
+        tree.resize(n);
+        this->n = n;
+    }
+
+    void update(int pos, int val) {
+        for (int i = pos; i < n; i += lowbit(i)) {
+            tree[i] = max(tree[i], val);
+        }
+    }
+
+    long long query(int pos) {
+        long long res = LLONG_MIN;
+        for (int i = pos; i >= 1; i -= lowbit(i)) {
+            res = max(res, tree[i]);
+        }
+        return res;
+    }
+
+    int lowbit(int x) {return x & (-x);}
+};
+
+
 class Solution {
 public:
     long long maxBalancedSubsequenceSum(vector<int>& nums) {
