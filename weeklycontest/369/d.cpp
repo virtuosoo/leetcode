e8dfd55d22f5d37871a0fe44212599e3d57d6e74
azusa jixiegeming <w572400957@163.com>
2023-11-11 21:12:08 +0800

    update

--- weeklycontest/369/d.cpp
+++ weeklycontest/369/d.cpp
@@ -12,9 +12,27 @@ public:
             g[v].push_back(u);
         }
 
+        auto dp = vector<vector<int> > (n, vector<int> (14, -1));
         function<int(int, int, int)> dfs = [&](int u, int fa, int t) {
+            if (t >= 14) {
+                return 0;
+            }
             
-        };
+            if (dp[u][t] != -1) {
+                return dp[u][t];
+            }
 
+            int res1 = 0, res2 = 0;
+            res1 += (coins[u] >> t) - k;
+            res2 += (coins[u] >> (t + 1));
+            for (int v : g[u]) {
+                if (v != fa) {
+                    res1 += dfs(v, u, t);
+                    res2 += dfs(v, u, t + 1);
+                }
+            }
+            return dp[u][t] = max(res1, res2);
+        };
+        return dfs(0, -1, 0);
     }
 };
