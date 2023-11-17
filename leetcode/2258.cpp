734080dbbb4ce14880399f2c02d90f70be79f5d2
azusa jixiegeming <w572400957@163.com>
2023-11-17 22:03:29 +0800

    update

--- leetcode/2258.cpp
+++ leetcode/2258.cpp
@@ -7,8 +7,8 @@ public:
     int maximumMinutes(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int> > reach(m, vector<int>(n ,0));
-        vector<vector<int> > vis(m, vector<int>(n ,0))
+        vector<vector<int> > reach(m, vector<int>(n ,-1)); //-1表示火不可达
+        vector<vector<int> > vis(m, vector<int>(n ,0));
         queue<tuple<int, int, int> > q;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -21,7 +21,8 @@ public:
 
         while (!q.empty()) {
             auto tp = q.front();
-            int x = tp[0], y = tp[1], t = tp[2];
+            q.pop();
+            auto [x, y, t] = tp;
             reach[x][y] = t;
             for (int i = 0; i < 4; ++i) {
                 int nx = x + dire[i][0], ny = y + dire[i][1];
@@ -31,5 +32,46 @@ public:
                 }
             }
         }
+
+        auto check = [&](int x) -> bool {
+            queue<tuple<int, int, int> > q;
+            q.push(make_tuple(0, 0, x));
+            vector<vector<int> > vis(m, vector<int>(n ,0));
+            vis[0][0] = 1;
+            while (!q.empty()) {
+                auto tp = q.front();
+                q.pop();
+                auto [x, y, t] = tp;
+                for (int i = 0; i < 4; ++i) {
+                    int nx = x + dire[i][0], ny = y + dire[i][1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && 
+                        !vis[nx][ny] && grid[nx][ny] == 0) {
+                        if (nx == m - 1 && ny == n - 1 && (reach[nx][ny] >= t + 1 || reach[nx][ny] == -1)) {
+                            return true;
+                        }
+                        if (reach[nx][ny] > t + 1 || reach[nx][ny] == -1) {
+                            vis[nx][ny] = 1;
+                            q.push(make_tuple(nx, ny, t + 1));
+                        }
+                    }
+                }
+            }
+            return false;
+        };
+
+        if (!check(0)) {
+            return -1;
+        }
+        int l = 0, r = 1e9;
+        while (l < r) {
+            int x = (l + r + 1) / 2;
+            //printf("%d %d\n", l, r);
+            if (check(x)) {
+                l = x;
+            } else {
+                r = x - 1;
+            }
+        }
+        return l;
     }
 };
