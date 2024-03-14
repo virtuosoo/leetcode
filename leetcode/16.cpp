11badeb5fdc854d4a95263a6128ec925db4c689a
wz xiaoxin <572400957@qq.com>
2024-03-14 17:25:53 +0800

    update

--- leetcode/16.cpp
+++ leetcode/16.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
+        int res, diff = 1e9;
+
         for (int i = 0; i < n - 2; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1, k = n - 1;
             while (j < k) {
-                if (nums[j] + nums[k] == -nums[i]) {
-                    vector<int> t{nums[i], nums[j], nums[k]};
-                    res.push_back(t);
-                    for (++j; j < k && nums[j] == nums[j - 1]; ++j);
-                    for (--k; j < k && nums[k] == nums[k + 1]; --k);
-                } else if (nums[j] + nums[k] < -nums[i]) {
+                if (abs(nums[j] + nums[k] - (target - nums[i])) < diff) {
+                    diff = abs(nums[j] + nums[k] - (target - nums[i]));
+                    res = nums[j] + nums[k] + nums[i];
+                } else if (nums[j] + nums[k] <= target - nums[i]) {
                     ++j;
                 } else {
                     --k;
