3b1795cda201d96cb8b14112563c61f8c0c84288
王梓 <w572400957@163.com>
2024-05-21 10:33:01 +0800

    update

---
+++ javaprog/myjava/lang/Object.java
@@ -0,0 +1,10 @@
+package myjava.lang;
+ 
+public class Object {
+    static {
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        Object o = new Object();
+    }
+}
