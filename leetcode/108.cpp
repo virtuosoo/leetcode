fec4f762d67535c675988fe476ceb2d37409e1d9
wz xiaoxin <572400957@qq.com>
2024-05-29 21:22:17 +0800

    update

---
+++ leetcode/108.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *dfs(vector<int> &nums, int start, int end) {
+        if (start > end) {
+            return nullptr;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = dfs(nums, start, mid - 1);
+        root->right = dfs(nums, mid + 1, end);
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return dfs(nums, 0, nums.size() - 1);
+    }
+};
